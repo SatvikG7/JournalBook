ef9a946bd2f1791491117c064a8d1fdf195ca5c9
JournalBook <141945870+JournalBook@users.noreply.github.com>
2023-09-06 21:38:17 +0530

    Add: projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,7 +10,7 @@ const projectsData = [
   {
     title: 'The Abhijit Chronicle',
     description: `Welcome to 'The Knowledge Unveiled,' a meticulously organized and comprehensive website dedicated to preserving the profound wisdom and insights shared by Abhijit Chavda during his captivating 'Ask Me Anything' live sessions and his videos on YouTube.`,
-    imgSrc: 'https://i.imgur.com/zBWDIFF.jpg',
+    imgSrc: '/static/images/zBWDIFF.jpg',
     href: 'https://www.journalbook.org/blog',
   },
 
